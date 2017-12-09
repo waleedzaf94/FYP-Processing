eb81fe05ecfd4c8a9529af019af694252292c19d
Waleed Zafar <waleedzaf94@gmail.com>
2017-12-09 14:29:25 +0800

    Initial Commit

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+using namespace std;
+
+int main() {
+	cout << "Input OBJ Filepath: " << endl;
+	string fname;
+	cin >> fname;
+	cout << "Input filename: " << fname << endl;
+    return 0;
+}
+
+
